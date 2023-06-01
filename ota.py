2b027adea5fe1f225b4e3dd6e3be0392644e32a0
Trin37099 <130125997+Trin37099@users.noreply.github.com>
2023-06-01 17:38:22 +0700

    ota.py

--- ota.py
+++ ota.py
@@ -523,7 +523,8 @@ if uploaded_files:
                         result = avg_adr.reset_index().pivot_table(values='ADR', index='Channel', columns='Room Type', fill_value=np.nan)
                         result.loc['Grand Total'] = result.mean()
                         result.at['Grand Total', 'Channel'] = 'Grand Total'
-                        result['ALL ROOM TYPE'] = result.mean(axis=1)
+                        avg_adr_all_room_type = df_january.groupby(['Channel'])['ADR'].mean()
+                        result['ALL ROOM TYPE'] = avg_adr_all_room_type
                         result = result.drop(columns='Channel')
                         result = result.applymap(lambda x: int(x) if not pd.isna(x) else np.nan)
                         st.write(result, use_container_width=True)
